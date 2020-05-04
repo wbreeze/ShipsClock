79cd23c41821ded48b7fecddba6417dda8654351
Douglas Lovell <doug@wbreeze.com>
2020-05-04 18:41:58 -0400

    Move model object to class level

--- ShipsClock/SceneDelegate.swift
+++ ShipsClock/SceneDelegate.swift
@@ -12,7 +12,7 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let shipsClock = ShipsClock()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -25,7 +25,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let shipsClock = ShipsClock()
             window.rootViewController = UIHostingController(rootView: contentView.environmentObject(shipsClock))
             self.window = window
             window.makeKeyAndVisible()
