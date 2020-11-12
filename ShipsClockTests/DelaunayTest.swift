6a463c995cfbd70f293f519d27244170317e4988
Douglas Lovell <doug@wbreeze.com>
2020-11-12 18:53:59 -0300

    Add test for Delaunay values

--- ShipsClockTests/DelaunayTest.swift
+++ ShipsClockTests/DelaunayTest.swift
@@ -20,24 +20,58 @@
 import XCTest
 
 class DelaunayTest: XCTestCase {
+    let prec = 1.0
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    struct TestCase {
+        let description : String
+        let julianDay : Double
+        let expectedD, expectedF, expectedL, expectedLP : Double
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testCases() -> [TestCase] {
+        [
+            TestCase(description: "17", julianDay: 1700000.5,
+                     expectedD: -65102318729347.000,
+                     expectedF: -9392305918095.756,
+                     expectedL: -394417025999805.688,
+                     expectedLP: -17368133160466.908),
+            TestCase(description: "19", julianDay: 1900000.5,
+                     expectedD:      -18888239804537.637,
+                     expectedF:       -2727013282919.186,
+                     expectedL:     -114422659629843.391,
+                     expectedLP:       -5038218972841.402),
+            TestCase(description: "21", julianDay: 2100000.5,
+                     expectedD:       -3119901613595.727,
+                     expectedF:        -452193947885.301,
+                     expectedL:      -18890849545993.520,
+                     expectedLP:        -831699978916.090),
+            TestCase(description: "23", julianDay: 2300000.5,
+                     expectedD:        -108886327503.672,
+                     expectedF:         -16782075763.288,
+                     expectedL:        -654020122585.479,
+                     expectedLP:         -28810296140.154),
+            TestCase(description: "25", julianDay: 2500000.5,
+                     expectedD:           -744642462.153,
+                     expectedF:            245657653.072,
+                     expectedL:          -6381280981.643,
+                     expectedLP:           -269606831.508)
+        ]
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        testCases().forEach { t in
+            let epoch = 2451545.0
+            let d = Delaunay(julianDay2000: t.julianDay - epoch)
+            XCTAssertEqual(d.d, t.expectedD, accuracy: prec,
+                           t.description + ":D")
+            XCTAssertEqual(d.f, t.expectedF, accuracy: prec,
+                           t.description + ":F")
+            XCTAssertEqual(d.l, t.expectedL, accuracy: prec,
+                           t.description + ":L")
+            XCTAssertEqual(d.lp, t.expectedLP, accuracy: prec,
+                           t.description + ":LP")
         }
     }
 
