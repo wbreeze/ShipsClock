c982a275f0266687eed03bf82bd75ca52c0bb877
Douglas Lovell <doug@wbreeze.com>
2020-11-14 20:57:07 -0300

    Compute Moon from MPP02 params given time

--- ShipsClock/MoonCalculator.swift
+++ ShipsClock/MoonCalculator.swift
@@ -32,7 +32,7 @@ import Foundation
  */
 struct MoonCalculator {
     static func sumOfProduct(_ a1:[Double], _ a2:[Double]) -> Double {
-        return zip(a1,a2).map { a,b in
+        return zip(a1,a2).map { a,b -> Double in
             a * b
         }.reduce(0.0, { a,b in
             a + b
@@ -47,19 +47,89 @@ struct MoonCalculator {
     }
     let epoch = 2451545.0 // calcs use base 2000-01-01 00:00 UT
     let cent = 36525.0 // 365.25 days / year * 100 years
-    let rad = (365.0 * 60.0 * 60.0) / Double.pi
+    let rad = (180.0 * 60.0 * 60.0) / Double.pi
+    let a405 = 384747.9613701725
+    let aelp = 384747.980674318
     let w = [3.81034, 8399.68473, -0.3e-04]
-    let mp = [
-        Perturbation(x:22639.58588, y:[ 2.35555,  8328.69142,  0.15e-3]),
-        Perturbation(x: 4586.43832, y:[ 8.04138,  7214.06287, -0.22e-3]),
-        Perturbation(x: 2369.91394, y:[10.39693, 15542.75429, -0.70e-4])
+    let p = 0.1e-04
+    let q = 0.11e-03
+    let mlonp = [
+        Perturbation(x: 22639.58588, y:[ 2.35555,  8328.69142,  0.15e-3]),
+        Perturbation(x:  4586.43832, y:[ 8.04138,  7214.06287, -0.22e-3]),
+        Perturbation(x:  2369.91394, y:[10.39693, 15542.75429, -0.70e-4])
     ]
-    
+    let mlatp = [
+        Perturbation(x: 18461.24006, y:[ 1.62791,  8433.46616, -0.60e-4]),
+        Perturbation(x:   999.69366, y:[ 0.72765,  -104.77473,  0.22e-3]),
+        Perturbation(x:  1010.16715, y:[ 3.98346, 16762.15758,  0.90e-4])
+    ]
+    let mdistp = [
+        Perturbation(x:385000.52904, y:[ 1.57080,     0.0    ,  0.0    ]),
+        Perturbation(x:-20905.35514, y:[ 3.92635,  8328.69142,  0.15e-3]),
+        Perturbation(x: -3699.11093, y:[ 9.61218,  7214.06287, -0.22e-3]),
+        Perturbation(x: -2955.96756, y:[11.96773, 15542.75429, -0.70e-4])
+    ]
+
     func longitude(julianDay jd: Double) -> Double {
         let tc = (jd - epoch) / cent
         let t = [1.0, tc, tc * tc]
-        return mp.reduce(0.0, { lon, term in
+        return mlonp.reduce(0.0, { lon, term in
             lon + term.value(t)
         }) / rad + MoonCalculator.sumOfProduct(w,t)
+    }
+    
+    func latitude(julianDay jd: Double) -> Double {
+        let tc = (jd - epoch) / cent
+        let t = [1.0, tc, tc * tc]
+        return mlatp.reduce(0.0, { lon, term in
+            lon + term.value(t)
+        }) / rad
+    }
+
+    func distance(julianDay jd: Double) -> Double {
+        let tc = (jd - epoch) / cent
+        let t = [1.0, tc, tc * tc]
+        return mdistp.reduce(0.0, { lon, term in
+            lon + term.value(t)
+        }) * a405 / aelp
+    }
+    
+    func location(julianDay jd: Double) -> ( x: Double, y: Double, z: Double ) {
+        let tc = (jd - epoch) / cent
+        let t = [1.0, tc, tc * tc]
+        
+        let lat = mlatp.reduce(0.0, { acc, term in
+            acc + term.value(t)
+        }) / rad
+        let lon = mlonp.reduce(0.0, { acc, term in
+            acc + term.value(t)
+        }) / rad + MoonCalculator.sumOfProduct(w,t)
+        let dist = mdistp.reduce(0.0, { acc, term in
+            acc + term.value(t)
+        }) * a405 / aelp
+        
+        let clamb = cos(lon)
+        let slamb = sin(lon)
+        let cbeta = cos(lat)
+        let sbeta = sin(lat)
+        let cw = dist * cbeta
+        let sw = dist * sbeta
+        let x1 = cw * clamb
+        let x2 = cw * slamb
+        let x3 = sw
+        let pw = p * tc
+        let qw = q * tc
+        let ra = 2.0 * sqrt(1 - pw * pw - qw * qw)
+        let pwqw = 2.0 * pw * qw
+        let pw2 = 1.0 - 2.0 * pw * pw
+        let qw2 = 1.0 - 2.0 * qw * qw
+        let pwra = pw * ra
+        let qwra = qw * ra
+
+        return (
+            x: pw2 * x1 + pwqw * x2 + pwra * x3,
+            y: pwqw * x1 + qw2 * x2 - qwra * x3,
+            z: -pwra * x1 + qwra * x2 + (pw2 + qw2 - 1.0) * x3
+        )
     }
 }
--- ShipsClockTests/MoonCalcTest.swift
+++ ShipsClockTests/MoonCalcTest.swift
@@ -21,7 +21,6 @@ import XCTest
 class MoonCalcTest: XCTestCase {
     let jules = Julian()
     let calculator = MoonCalculator()
-    let prec = 1.0
     
     struct LongitudeTestCase {
         let julianDay: Double
@@ -33,13 +32,13 @@ class MoonCalcTest: XCTestCase {
     func lonTests() -> [LongitudeTestCase] {
         [
             LongitudeTestCase(
-                julianDay: 1700000.5,
-                azimuth: Arcs.dmsToAngle(101, 38, 52.5),
-                rightAscension: Arcs.hmsToAngle(18, 22, 40),
-                hourAngle: Arcs.hmsToAngle(6, 40, 4),
-                eclipticLongitude: -172829.58663,
-                galacticLongitude: Arcs.dmsToAngle(7, 49, 49.6),
-                description: "July 31, 2020"),
+                julianDay: jules.julianDay(2019, 10, 2, 12, 0, 0),
+                azimuth: Arcs.dmsToAngle(111, 42, 10.9),
+                rightAscension: Arcs.hmsToAngle(15, 58, 12),
+                hourAngle: Arcs.hmsToAngle(6, 59, 12),
+                eclipticLongitude: Arcs.dmsToAngle(240, 47, 47.0),
+                galacticLongitude: Arcs.dmsToAngle(354, 36, 47.8),
+                description: "October 2, 2019"),
             LongitudeTestCase(
                 julianDay: jules.julianDay(2020, 7, 31, 16, 0, 0),
                 azimuth: Arcs.dmsToAngle(101, 38, 52.5),
@@ -55,14 +54,64 @@ class MoonCalcTest: XCTestCase {
                 hourAngle: Arcs.hmsToAngle(08, 27, 17),
                 eclipticLongitude: Arcs.dmsToAngle(301, 24, 34.8),
                 galacticLongitude: Arcs.dmsToAngle(19, 56, 23.9),
-                description: "August 2, 2020")
+                description: "August 2, 2020"),
+            LongitudeTestCase(
+                julianDay: jules.julianDay(2020,11, 1, 12, 0, 0),
+                azimuth: Arcs.dmsToAngle(266, 55, 51.1),
+                rightAscension: Arcs.hmsToAngle(3, 6, 55),
+                hourAngle: Arcs.hmsToAngle(7, 53, 21),
+                eclipticLongitude: Arcs.dmsToAngle(48, 34, 48.5),
+                galacticLongitude: Arcs.dmsToAngle(164, 34, 20.9),
+                description: "November 1, 2020"),
+            LongitudeTestCase(
+                julianDay: jules.julianDay(2020,11, 14, 12, 0, 0),
+                azimuth: Arcs.dmsToAngle(71, 00, 37.6),
+                rightAscension: Arcs.hmsToAngle(14, 46, 23),
+                hourAngle: Arcs.hmsToAngle(2, 54, 57),
+                eclipticLongitude: Arcs.dmsToAngle(222, 53, 32.5),
+                galacticLongitude: Arcs.dmsToAngle(341, 52, 23.4),
+                description: "November 14, 2020")
         ]
     }
     
     func testLongitude() throws {
+        let prec = 5.0
         lonTests().forEach { t in
-            let lon = calculator.longitude(julianDay: t.julianDay)
-            XCTAssertEqual(lon, t.eclipticLongitude, accuracy: prec, t.description)
+            let loc = calculator.location(julianDay: t.julianDay)
+            let sc = Arcs.spherical(x: loc.x, y: loc.y, z: loc.z)
+            XCTAssertEqual(sc.azimuth, t.eclipticLongitude, accuracy: prec, t.description)
+        }
+    }
+    
+    struct XyzTestCase {
+        var jd, x, y, z: Double
+        func description() -> String {
+            "jd: #{jd}"
+        }
+    }
+    
+    func xyzTests() -> [XyzTestCase] {
+        [
+            XyzTestCase(jd: 2444239.5,
+                        x: 43890.2824005, y:381188.7274523, z:-31633.3816524),
+            XyzTestCase(jd: 2446239.5,
+                        x: -313664.5964499, y: 212007.2667385, z: 33744.7512039),
+            XyzTestCase(jd: 2448239.5,
+                        x: -273220.0606714, y: -296859.7682229, z: -34604.3569962),
+            XyzTestCase(jd: 2450239.5,
+                        x: 171613.1427993, y: -318097.3375025, z: 31293.5482404),
+            XyzTestCase(jd: 2452239.5,
+                        x: 396530.0063512, y: 47487.9224886, z: -36085.3090343)
+        ]
+    }
+    
+    func testXYZ() throws {
+        let precp = 20.0
+        xyzTests().forEach { t in
+            let xyz = calculator.location(julianDay: t.jd)
+            XCTAssertEqual(xyz.x, t.x, accuracy: t.x / precp, "X of \(t.description())")
+            XCTAssertEqual(xyz.y, t.y, accuracy: t.y / precp, "Y of \(t.description())")
+            XCTAssertEqual(xyz.z, t.z, accuracy: t.z / precp, "Z of \(t.description())")
         }
     }
 }
