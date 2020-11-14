32f6a83cc39c43758e1ad6de829a77807d6404a9
Douglas Lovell <doug@wbreeze.com>
2020-11-13 23:51:12 -0300

    Refactor computation using precomputed parameters

--- ShipsClock/MoonCalculator.swift
+++ ShipsClock/MoonCalculator.swift
@@ -16,6 +16,8 @@
    limitations under the License.
   */
 
+import Foundation
+
 /*
  Moon longitude calculation following:
  The lunar theory ELP revisited. Introduction of new planetary perturbations
@@ -29,25 +31,35 @@
  Jean CHAPRONT and G ́erard FRANCOU Observatoire de Paris -SYRTE department - UMR 8630/CNRS October 2002
  */
 struct MoonCalculator {
-    let epoch = 2451545.0 // calcs use base 2000-01-01 00:00 UT
-    
-    let delaunay: Delaunay
-    let time: Double
-    
-    init(julianDay jd: Double) {
-        time = jd - epoch
-        delaunay = Delaunay(julianDay2000: time)
+    static func sumOfProduct(_ a1:[Double], _ a2:[Double]) -> Double {
+        return zip(a1,a2).map { a,b in
+            a * b
+        }.reduce(0.0, { a,b in
+            a + b
+        })
     }
+    struct Perturbation {
+        var x: Double
+        var y: [Double]
+        func value(_ t: [Double]) -> Double {
+            x * sin(MoonCalculator.sumOfProduct(y,t))
+        }
+    }
+    let epoch = 2451545.0 // calcs use base 2000-01-01 00:00 UT
+    let cent = 36525.0 // 365.25 days / year * 100 years
+    let rad = (365.0 * 60.0 * 60.0) / Double.pi
+    let w = [3.81034, 8399.68473, -0.3e-04]
+    let mp = [
+        Perturbation(x:22639.58588, y:[ 2.35555,  8328.69142,  0.15e-3]),
+        Perturbation(x: 4586.43832, y:[ 8.04138,  7214.06287, -0.22e-3]),
+        Perturbation(x: 2369.91394, y:[10.39693, 15542.75429, -0.70e-4])
+    ]
     
-    func longitude() -> Double {
-        return Arcs.normalizedDegrees(
-            for: MainTerms.mainLon.reduce(
-                0.0,
-                { lon, term in
-                    return lon + term.value(delaunay)
-                }
-            )
-            + delaunay.w1
-        )
+    func longitude(julianDay jd: Double) -> Double {
+        let tc = (jd - epoch) / cent
+        let t = [1.0, tc, tc * tc]
+        return mp.reduce(0.0, { lon, term in
+            lon + term.value(t)
+        }) / rad + MoonCalculator.sumOfProduct(w,t)
     }
 }
--- ShipsClockTests/MoonCalcTest.swift
+++ ShipsClockTests/MoonCalcTest.swift
@@ -20,6 +20,7 @@ import XCTest
 
 class MoonCalcTest: XCTestCase {
     let jules = Julian()
+    let calculator = MoonCalculator()
     let prec = 1.0
     
     struct LongitudeTestCase {
@@ -31,6 +32,14 @@ class MoonCalcTest: XCTestCase {
     
     func lonTests() -> [LongitudeTestCase] {
         [
+            LongitudeTestCase(
+                julianDay: 1700000.5,
+                azimuth: Arcs.dmsToAngle(101, 38, 52.5),
+                rightAscension: Arcs.hmsToAngle(18, 22, 40),
+                hourAngle: Arcs.hmsToAngle(6, 40, 4),
+                eclipticLongitude: -172829.58663,
+                galacticLongitude: Arcs.dmsToAngle(7, 49, 49.6),
+                description: "July 31, 2020"),
             LongitudeTestCase(
                 julianDay: jules.julianDay(2020, 7, 31, 16, 0, 0),
                 azimuth: Arcs.dmsToAngle(101, 38, 52.5),
@@ -52,9 +61,9 @@ class MoonCalcTest: XCTestCase {
     
     func testLongitude() throws {
         lonTests().forEach { t in
-            let calculator = MoonCalculator(julianDay: t.julianDay)
-            let lon = calculator.longitude()
+            let lon = calculator.longitude(julianDay: t.julianDay)
             XCTAssertEqual(lon, t.eclipticLongitude, accuracy: prec, t.description)
         }
     }
 }
+
